bb14f741626cd1363e51f2cf4b27e6f0fae0b4e2
IlahaRahman <ilahe.rahmanzadeh@icloud.com>
2024-12-30 03:37:33 +0100

    Created 2 layouts based on Figma

---
+++ Lesson 2/app/src/main/java/com/mobileapps/lesson2/AccountActivity.kt
@@ -0,0 +1,15 @@
+package com.mobileapps.lesson2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+
+    }
+
+
+}
---
+++ Lesson 2/app/src/main/java/com/mobileapps/lesson2/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.mobileapps.lesson2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+    }
+
+}
