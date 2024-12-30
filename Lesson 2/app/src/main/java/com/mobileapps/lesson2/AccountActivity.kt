a3cc698007b3bd82fd190e0690386832c4a23253
IlahaRahman <ilahe.rahmanzadeh@icloud.com>
2024-12-31 00:51:07 +0100

    Added Login and Switch Activities into Fragments

--- Lesson 2/app/src/main/java/com/mobileapps/lesson2/AccountActivity.kt
+++ Lesson 2/app/src/main/java/com/mobileapps/lesson2/AccountActivity.kt
@@ -2,51 +2,26 @@ package com.mobileapps.lesson2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
-import android.content.Intent
-import android.widget.Toast
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputLayout
+import androidx.fragment.app.Fragment
+
 
 class AccountActivity : AppCompatActivity() {
 
-    private lateinit var emailTextInputLayout: TextInputLayout
-    private lateinit var passwordTextInputLayout: TextInputLayout
-    private lateinit var nextButton: MaterialButton
-
-    private val credentialsManager = CredentialsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
 
-        emailTextInputLayout = findViewById(R.id.emailTextInputLayout)
-        passwordTextInputLayout = findViewById(R.id.passwordTextInputLayout)
-        nextButton = findViewById(R.id.nextButton)
-        val registerNowTextView = findViewById<TextView>(R.id.registerNowTextView)
-
-        registerNowTextView.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
-        nextButton.setOnClickListener {
-            validateAndLogin()
+        if (savedInstanceState == null) {
+            replaceFragment(LoginFragment())
         }
 
     }
-    private fun validateAndLogin (){
-        val email = emailTextInputLayout.editText?.text.toString().trim()
-        val password = passwordTextInputLayout.editText?.text.toString().trim()
 
-        if (!credentialsManager.isEmailValid(email)) {
-            Toast.makeText(this, "Invalid email format", Toast.LENGTH_SHORT).show()
-        } else if (!credentialsManager.isPasswordValid(password)) {
-            Toast.makeText(this, "Password cannot be empty", Toast.LENGTH_SHORT).show()
-        } else {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent) }
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .addToBackStack(null)
+            .commit()
     }
-
-
 }
--- Lesson 2/app/src/main/java/com/mobileapps/lesson2/SignUpActivity.kt
+++ Lesson 2/app/src/main/java/com/mobileapps/lesson2/SignUpActivity.kt
@@ -1,68 +1,26 @@
 package com.mobileapps.lesson2
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputLayout
+import androidx.fragment.app.Fragment
+
 
 class SignUpActivity : AppCompatActivity() {
-
-    private lateinit var emailTextInputLayout: TextInputLayout
-    private lateinit var passwordTextInputLayout: TextInputLayout
-    private lateinit var registerButton: MaterialButton
-    private lateinit var loginTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        emailTextInputLayout = findViewById(R.id.emailTextInputLayout)
-        passwordTextInputLayout = findViewById(R.id.passwordTextInputLayout)
-        registerButton = findViewById(R.id.nextButton)
-        loginTextView = findViewById(R.id.loginTextView)
-
-        loginTextView.setOnClickListener {
-            val intent = Intent(this, AccountActivity::class.java)
-            startActivity(intent)
+        if (savedInstanceState == null) {
+            replaceFragment(RegisterFragment())
         }
 
-        registerButton.setOnClickListener {
-            registerAccount()
-        }
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 
-    private fun registerAccount() {
-        val email = emailTextInputLayout.editText?.text.toString().trim()
-        val password = passwordTextInputLayout.editText?.text.toString().trim()
-        var isValid = true
 
-        if (!CredentialsManager.isEmailValid(email)) {
-            emailTextInputLayout.error = "Invalid email format"
-            isValid = false
-        } else {
-            emailTextInputLayout.error = null
-        }
-
-        if (!CredentialsManager.isPasswordValid(password)) {
-            passwordTextInputLayout.error = "Password cannot be empty"
-            isValid = false
-        } else {
-            passwordTextInputLayout.error = null
-        }
-
-        if (isValid) {
-            val isRegistered = CredentialsManager.register(email, password)
-            if (isRegistered) {
-                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, AccountActivity::class.java)
-                startActivity(intent)
-                finish()
-            } else {
-                Toast.makeText(this, "Email is already taken", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
 }
